b71e0b922746f432733e81c07e318e03c32440c2
Alex-Fes <aleksei.fesenko86@gmail.com>
2022-05-24 16:19:56 +0300

    change src

---
+++ src/components/accordion/accordion.tsx
@@ -0,0 +1,39 @@
+
+type AccordionPropsType = {
+    collapsed: Boolean;
+    title: string
+
+}
+
+function Accordion(props: AccordionPropsType) {
+    console.log('Accordion rendering')
+    //debugger
+    if (props.collapsed === true) {
+    return <div>
+        <AccordionTitle title = {props.title} collapsed/>
+        <AccordionBody/>
+    </div>
+    } else {
+        return <div>
+            <AccordionTitle title = {props.title} collapsed/>
+        </div>
+    }
+}
+
+function AccordionTitle(props: AccordionPropsType) {
+    console.log('AccordionTitle rendering')
+    //debugger
+    return <h3>{props.title}</h3>
+}
+
+function AccordionBody() {
+    console.log('AccordionBody rendering')
+    //debugger
+    return <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+    </ul>
+}
+
+export default Accordion;
