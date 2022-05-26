5c0693d4e70b58d207bff212cd3a360af891e42b
Alex-Fes <aleksei.fesenko86@gmail.com>
2022-05-26 23:17:46 +0300

    create social network

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
-  );
+
+const App = () => {
+    return (
+        <div className='app-wrapper'>
+            <header className='header'>
+                <img src="https://spng.subpng.com/20180317/vie/kisspng-psychology-mind-psychologist-clip-art-brain-relaxing-cliparts-5aad8010187c63.4954245515213199521003.jpg"/>
+            </header>
+            <nav className='nav'>
+                <div><a >Profile</a></div>
+                <div><a >Message</a></div>
+                <div><a >News</a></div>
+                <div><a >Music</a></div>
+                <div><a >Settings</a></div>
+            </nav>
+            <div className='content'>Main content</div>
+
+        </div>
+    );
 }
+
 
 export default App;
