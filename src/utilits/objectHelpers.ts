2111d5f4e81f0aeb0b24bae2bdcfe4d78e702ce7
Alex-Fes <aleksei.fesenko86@gmail.com>
2022-12-27 23:13:26 +0100

    add: updateObjectInArray for method map each use in reducer

---
+++ src/utilits/objectHelpers.ts
@@ -0,0 +1,15 @@
+import {UserType} from "../Redux/usersReducer";
+
+export const updateObjectInArray = (items: Array<UserType>,
+                                    itemId: number,
+                                    objPropName: string,
+                                    newObjProps: any) => {
+    return items.map(u => {
+            // @ts-ignore
+        if (u[objPropName] === itemId) {
+                return {...u, ...newObjProps}
+            }
+            return u
+        }
+    )
+}
