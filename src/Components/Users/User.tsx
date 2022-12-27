702df73912b35e90717bc053bc606b922e75ddc9
Alex-Fes <aleksei.fesenko86@gmail.com>
2022-12-27 23:34:36 +0100

    style: add ESLint

--- src/Components/Users/User.tsx
+++ src/Components/Users/User.tsx
@@ -1,44 +1,63 @@
-import React from 'react';
-import styles from './Users.module.css'
+import React from 'react'
+
+import { NavLink } from 'react-router-dom'
+
 import userPhoto from '../../assets/images/userPic.png'
-import {UserType} from '../../Redux/usersReducer';
-import {NavLink} from "react-router-dom";
+import { UserType } from '../../Redux/usersReducer'
+
+import styles from './Users.module.css'
 
 export const User = (props: UserPropsType) => {
-    return <div>
-                <span>
-                    <div>
-                        <NavLink to={'/profile/' + props.user.id}>
-                    <img src={props.user.photos.small != null ? props.user.photos.small : userPhoto}
-                         alt="UserPic" className={styles.userPhoto}/>
-                            </NavLink>
-                </div>
-                    <div>{props.user.followed ?
-                        <button disabled={props.followingInProcess.some(id => id === props.user.id)} onClick={() => {
-                            props.unfollow(props.user.id)
-                        }}>UnFollow</button>
-                        : <button disabled={props.followingInProcess.some(id => id === props.user.id)} onClick={() => {
-                            props.follow(props.user.id)
-                        }}>Follow</button>}
-                </div>
-                </span>
-        <span>
-                        <div>{props.user.name}</div>
-                        <div>{props.user.status}</div>
-                    </span>
-        <span>
-                     <div>{'u.location.country'}</div>
-                        <div>{'u.location.city'}</div>
-                </span>
-
+  return (
+    <div>
+      <span>
+        <div>
+          <NavLink to={'/profile/' + props.user.id}>
+            <img
+              src={props.user.photos.small != null ? props.user.photos.small : userPhoto}
+              alt="UserPic"
+              className={styles.userPhoto}
+            />
+          </NavLink>
+        </div>
+        <div>
+          {props.user.followed ? (
+            <button
+              disabled={props.followingInProcess.some(id => id === props.user.id)}
+              onClick={() => {
+                props.unfollow(props.user.id)
+              }}
+            >
+              UnFollow
+            </button>
+          ) : (
+            <button
+              disabled={props.followingInProcess.some(id => id === props.user.id)}
+              onClick={() => {
+                props.follow(props.user.id)
+              }}
+            >
+              Follow
+            </button>
+          )}
+        </div>
+      </span>
+      <span>
+        <div>{props.user.name}</div>
+        <div>{props.user.status}</div>
+      </span>
+      <span>
+        <div>{'u.location.country'}</div>
+        <div>{'u.location.city'}</div>
+      </span>
     </div>
+  )
 }
 
 type UserPropsType = {
-    user: UserType
-    followingInProcess: Array<number>
-    unfollow: (id: number) => void
-    follow: (id: number) => void
-    key: number
-
+  user: UserType
+  followingInProcess: Array<number>
+  unfollow: (id: number) => void
+  follow: (id: number) => void
+  key: number
 }
--- src/Components/Users/Users.tsx
+++ src/Components/Users/Users.tsx
@@ -1,31 +1,40 @@
-import React from 'react';
-import {InitialStateType} from '../../Redux/usersReducer';
-import {Pagination} from "../common/Pagination/Pagination";
-import {User} from "./User";
+import React from 'react'
+
+import { InitialStateType } from '../../Redux/usersReducer'
+import { Pagination } from '../common/Pagination/Pagination'
+
+import { User } from './User'
 
 export const Users = (props: UsersPropsType) => {
-    return <div>
-        <Pagination currentPage={props.currentPage}
-                    onPageChanged={props.onPageChanged}
-                    totalUserCount={props.totalUserCount}
-                    pageSize={props.pageSize}/>
+  return (
+    <div>
+      <Pagination
+        currentPage={props.currentPage}
+        onPageChanged={props.onPageChanged}
+        totalUserCount={props.totalUserCount}
+        pageSize={props.pageSize}
+      />
 
-        {props.usersPage.users.map(u =>
-            <User key={u.id} user={u}
-                  followingInProcess={props.followingInProcess}
-                  follow={props.follow}
-                  unfollow={props.unfollow}/>
-        )}
+      {props.usersPage.users.map(u => (
+        <User
+          key={u.id}
+          user={u}
+          followingInProcess={props.followingInProcess}
+          follow={props.follow}
+          unfollow={props.unfollow}
+        />
+      ))}
     </div>
+  )
 }
 
 type UsersPropsType = {
-    usersPage: InitialStateType
-    totalUserCount: number
-    pageSize: number
-    currentPage: number
-    onPageChanged: (pageNumber: number) => void
-    follow: (userId: number) => void
-    unfollow: (userId: number) => void
-    followingInProcess: Array<number>
+  usersPage: InitialStateType
+  totalUserCount: number
+  pageSize: number
+  currentPage: number
+  onPageChanged: (pageNumber: number) => void
+  follow: (userId: number) => void
+  unfollow: (userId: number) => void
+  followingInProcess: Array<number>
 }
