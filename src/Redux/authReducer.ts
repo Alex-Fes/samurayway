1b83d02485274cb60b4de29828d1226c2b59e470
Alex-Fes <aleksei.fesenko86@gmail.com>
2022-12-27 23:11:00 +0100

    refactor: include async/await - try/catch to thunks

--- src/Redux/authReducer.ts
+++ src/Redux/authReducer.ts
@@ -4,22 +4,8 @@ import {stopSubmit} from "redux-form";
 import {AppThunkType} from "./redux-store";
 import {Dispatch} from "redux";
 
-const SET_USER_DATA = 'SET-USER-DATA';
-const SET_LOGIN_DATA = 'SET-LOGIN-DATA';
-
-export type InitialStateType = {
-    userId: number
-    email: string
-    login: string
-    isAuth: boolean
-};
-
-let initialState: InitialStateType = {
-    userId: 0,
-    email: '',
-    login: '',
-    isAuth: false
-};
+const SET_USER_DATA = 'auth/SET-USER-DATA';
+const SET_LOGIN_DATA = 'auth/SET-LOGIN-DATA';
 
 export const authReducer = (state: InitialStateType = initialState, action: ActionTypes): InitialStateType => {
     switch (action.type) {
@@ -33,49 +19,71 @@ export const authReducer = (state: InitialStateType = initialState, action: Acti
     }
 
 }
+
+//ACTIONS ======================================
 export const setAuthUserDataAC = (userId: number, email: string, login: string, isAuth: boolean) =>
     ({type: SET_USER_DATA, payload: {userId, email, login, isAuth}}) as const;
 
-export const getAuthUserDataTC: any = () => {
-    return (dispatch: Dispatch) => {
-       return  authAPI.me().then(response => {
+
+//THUNKS =======================================
+
+export const getAuthUserDataTC: any = () =>
+    async (dispatch: Dispatch) => {
+        try {
+            let response = await authAPI.me();
             if (response.data.resultCode === 0) {
                 let {id, email, login} = response.data.data;
                 dispatch(setAuthUserDataAC(id, email, login, true))
             }
-        })
+        } catch (err) {
+            console.log(err)
+        }
     }
-}
 
-export const loginUserTC = (email: string, password: string, rememberMe: boolean): AppThunkType => {
-    return (dispatch) => {
-        authAPI.login(email, password, rememberMe)
-            .then(response => {
-                if (response.data.resultCode === 0) {
-                    dispatch(getAuthUserDataTC())
-                } else {
-                    let message = response.data.messages.length > 0 ? response.data.messages[0] : 'Some error';
-                    dispatch(stopSubmit("login", {_error: message}))
-                }
-            })
+
+export const loginUserTC = (email: string, password: string, rememberMe: boolean): AppThunkType =>
+    async (dispatch) => {
+        try {
+            let response = await authAPI.login(email, password, rememberMe)
+            if (response.data.resultCode === 0) {
+                dispatch(getAuthUserDataTC())
+            } else {
+                let message = response.data.messages.length > 0 ? response.data.messages[0] : 'Some error';
+                dispatch(stopSubmit("login", {_error: message}))
+            }
+        } catch (err) {
+            console.log(err)
+        }
     }
-}
 
-export const logoutUserTC = (): AppThunkType => {
-    return (dispatch) => {
-        authAPI.logout()
-            .then(response => {
-                if (response.data.resultCode === 0) {
-                    dispatch(setAuthUserDataAC(0, '', '', false))
-                }
-            })
+
+export const logoutUserTC = (): AppThunkType =>
+    async (dispatch) => {
+        try {
+            let response = await authAPI.logout()
+            if (response.data.resultCode === 0) {
+                dispatch(setAuthUserDataAC(0, '', '', false))
+            }
+        } catch (err) {
+            console.log(err)
+        }
     }
-}
 
 
+//TYPES ========================================
+export type InitialStateType = {
+    userId: number
+    email: string
+    login: string
+    isAuth: boolean
+};
 
-
-
+let initialState: InitialStateType = {
+    userId: 0,
+    email: '',
+    login: '',
+    isAuth: false
+};
 
 
 
--- src/Redux/profileReducer.tsx
+++ src/Redux/profileReducer.tsx
@@ -11,32 +11,6 @@ const SET_USER_PROFILE = 'SET-USER-PROFILE';
 const SET_STATUS = 'SET-STATUS';
 const DELETE_POST = 'DELETE-POST';
 
-export type PostType = {
-    id: number
-    message: string
-    likeCount: number
-}
-export type ProfileType = {
-    aboutMe: '',
-    contacts: {
-        facebook: '',
-        website: '',
-        vk: '',
-        twitter: '',
-        instagram: '',
-        youtube: '',
-        github: '',
-        mainLink: '',
-    },
-    lookingForAJob: false,
-    lookingForAJobDescription: '',
-    fullName: '',
-    userId: 0,
-    photos: {
-        small: '',
-        large: '',
-    }
-}
 
 export type InitialStateType = typeof initialState;
 let initialState = {
@@ -89,12 +63,16 @@ export const profileReducer = (state: InitialStateType = initialState, action: A
         case SET_STATUS:
             return {...state, status: action.status} as InitialStateType;
         case DELETE_POST:
-            return {...state,
-                posts: state.posts.filter(post => post.id !== action.postId)} as InitialStateType;
+            return {
+                ...state,
+                posts: state.posts.filter(post => post.id !== action.postId)
+            } as InitialStateType;
         default:
             return state;
     }
 }
+
+//ACTIONS ================================
 export const addPostActionCreator = (newPostText: string) => ({type: ADD_POST, newPostText}) as const;
 //export const onChangePostActionCreator = (newText: string) => ({type: CHANGE_NEW_TEXT, newText: newText}) as const;
 export const setUserProfile = (profile: RootUserProfileType) => ({type: SET_USER_PROFILE, profile}) as const;
@@ -102,34 +80,70 @@ export const setStatus = (status: string) => ({type: SET_STATUS, status}) as con
 export const deletePostAC = (postId: number) => ({type: DELETE_POST, postId}) as const;
 
 
+//THUNKS ==================================
+export const getUserProfile = (userId: string): AppThunkType =>
+    async (dispatch) => {
+        try {
+            let response = await usersAPI.getProfile(userId)
+            dispatch(setUserProfile(response.data))
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
+export const getStatus = (userId: string): AppThunkType =>
+    async (dispatch) => {
+        try {
+            let response = await profileAPI.getStatus(userId)
+            dispatch(setStatus(response.data))
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
+export const updateStatus = (status: string): AppThunkType =>
+    async (dispatch) => {
+        try {
+            let response = await profileAPI.updateStatus(status)
+            if (response.data.resultCode === 0)
+                dispatch(setStatus(status))
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
+
+//TYPES ==========================
+export type PostType = {
+    id: number
+    message: string
+    likeCount: number
+}
+export type ProfileType = {
+    aboutMe: '',
+    contacts: {
+        facebook: '',
+        website: '',
+        vk: '',
+        twitter: '',
+        instagram: '',
+        youtube: '',
+        github: '',
+        mainLink: '',
+    },
+    lookingForAJob: false,
+    lookingForAJobDescription: '',
+    fullName: '',
+    userId: 0,
+    photos: {
+        small: '',
+        large: '',
+    }
+}
+
 type ProfileStatusType = {
     status: string
 }
-export const getUserProfile = (userId: string): AppThunkType => {
-    return (dispatch) => {
-        usersAPI.getProfile(userId).then(response => {
-            dispatch(setUserProfile(response.data))
-        })
-    }
-}
-export const getStatus = (userId: string): AppThunkType => {
-    return (dispatch) => {
-        profileAPI.getStatus(userId).then(response => {
-            dispatch(setStatus(response.data))
-        })
-    }
-}
-export const updateStatus = (status: string): AppThunkType => {
-    return (dispatch) => {
-        profileAPI.updateStatus(status).then(response => {
-            if (response.data.resultCode === 0)
-                dispatch(setStatus(status))
-        })
-    }
-}
-
-
-
 
 
 
