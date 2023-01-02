02ba784b3b913205a573c23b4c06aff3bbc6d93e
Alex-Fes <aleksei.fesenko86@gmail.com>
2023-01-02 20:27:25 +0100

    style: add ESLint

--- src/Redux/appReducer.ts
+++ src/Redux/appReducer.ts
@@ -1,53 +1,40 @@
-import {ActionTypes} from "./state";
-import {Dispatch} from "redux";
-import {getAuthUserDataTC} from "./authReducer";
-import {AppThunkType} from "./redux-store";
+import { Dispatch } from 'redux'
 
-const INITIALIZED_SUCCESS = 'INITIALIZED-SUCCESS';
+import { getAuthUserDataTC } from './authReducer'
+import { AppThunkType } from './redux-store'
+import { ActionTypes } from './state'
+
+const INITIALIZED_SUCCESS = 'INITIALIZED-SUCCESS'
 
 let initialState: InitialStateType = {
-    initialized: false
-};
-export const appReducer = (state: InitialStateType = initialState, action: ActionTypes): InitialStateType => {
-    switch (action.type) {
-        case INITIALIZED_SUCCESS:
-            return {
-                ...state,
-                initialized: true
-            }
-        default:
-            return state
-    }
+  initialized: false,
 }
-export const initializedSuccessAC = () => ({type: INITIALIZED_SUCCESS}) as const;
+
+export const appReducer = (
+  state: InitialStateType = initialState,
+  action: ActionTypes
+): InitialStateType => {
+  switch (action.type) {
+    case INITIALIZED_SUCCESS:
+      return {
+        ...state,
+        initialized: true,
+      }
+    default:
+      return state
+  }
+}
+export const initializedSuccessAC = () => ({ type: INITIALIZED_SUCCESS } as const)
 
 export const initializeAppTC = (): AppThunkType => {
-    return (dispatch: Dispatch) => {
-        dispatch(getAuthUserDataTC()).then(()=>{
-            dispatch(initializedSuccessAC())
-        });
-    }
+  return (dispatch: Dispatch) => {
+    dispatch(getAuthUserDataTC()).then(() => {
+      dispatch(initializedSuccessAC())
+    })
+  }
 }
 
 //Types
 export type InitialStateType = {
-    initialized: boolean
-};
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  initialized: boolean
+}
