94447032f52cfd8623f741a41ddec69e09253a70
Alex-Fes <aleksei.fesenko86@gmail.com>
2023-01-02 20:27:18 +0100

    style: add ESLint

--- src/App.tsx
+++ src/App.tsx
@@ -1,71 +1,71 @@
-import React from 'react';
-import './App.css';
-import Navigation from "./Components/Navigation/Navigation";
-import {Route, withRouter} from "react-router-dom";
-import News from "./Components/News/News";
-import Music from "./Components/Music/Music";
-import Settings from "./Components/Settings/Settings";
-import Sidebar from "./Components/Sidebar/Sidebar";
-import Footer from "./Components/Footer/Footer";
-import DialogsContainer from "./Components/Dialogs/DialogsContainer";
-import UsersContainer from "./Components/Users/UsersContainer";
-import ProfileContainer from "./Components/Profile/ProfileContainer";
-import HeaderContainer from "./Components/Header/HeaderContainer";
-import Login from "./Components/Login/Login";
-import {connect} from "react-redux";
-import {compose} from "redux";
-import {initializeAppTC} from "./Redux/appReducer";
-import {StoreType} from "./Redux/redux-store";
-import {Preloader} from "./Components/common/Preloader/Preloader";
+import React from 'react'
 
+import './App.css'
+import { connect } from 'react-redux'
+import { Route, withRouter } from 'react-router-dom'
+import { compose } from 'redux'
+
+import { Preloader } from './Components/common/Preloader/Preloader'
+import DialogsContainer from './Components/Dialogs/DialogsContainer'
+import Footer from './Components/Footer/Footer'
+import HeaderContainer from './Components/Header/HeaderContainer'
+import Login from './Components/Login/Login'
+import Music from './Components/Music/Music'
+import Navigation from './Components/Navigation/Navigation'
+import News from './Components/News/News'
+import ProfileContainer from './Components/Profile/ProfileContainer'
+import Settings from './Components/Settings/Settings'
+import Sidebar from './Components/Sidebar/Sidebar'
+import UsersContainer from './Components/Users/UsersContainer'
+import { initializeAppTC } from './Redux/appReducer'
+import { StoreType } from './Redux/redux-store'
 
 class App extends React.Component<AppPropsType> {
-    componentDidMount() {
-        this.props.initializeAppTC();
-
+  componentDidMount() {
+    this.props.initializeAppTC()
+  }
+  render() {
+    if (!this.props.initialized) {
+      return <Preloader />
     }
-    render() {
-        if(!this.props.initialized) {
-            return <Preloader/>
-        }
 
-        return (
-            <div className='app-wrapper'>
-                <HeaderContainer/>
-                <Navigation/>
+    return (
+      <div className="app-wrapper">
+        <HeaderContainer />
+        <Navigation />
 
-                <div className='app-wrapper-content'>
-                    <Route path='/profile/:userId?' render={() => <ProfileContainer/>}></Route>
-                    <Route path='/dialogs'
-                           render={() => <DialogsContainer/>}></Route>
-                    <Route path='/news' render={() => <News/>}></Route>
-                    <Route path='/music' render={() => <Music/>}></Route>
-                    <Route path='/users' render={() => <UsersContainer/>}></Route>
-                    <Route path='/settings' render={() => <Settings/>}></Route>
-                    <Route path='/sidebar' render={() => <Sidebar/>}></Route>
-                    <Route path='/login' render={() => <Login/>}></Route>
-
-                </div>
-                <div className='footer'>
-                    <Route render={() => <Footer/>}></Route>
-                </div>
-            </div>)
-    }
+        <div className="app-wrapper-content">
+          <Route path="/profile/:userId?" render={() => <ProfileContainer />}></Route>
+          <Route path="/dialogs" render={() => <DialogsContainer />}></Route>
+          <Route path="/news" render={() => <News />}></Route>
+          <Route path="/music" render={() => <Music />}></Route>
+          <Route path="/users" render={() => <UsersContainer />}></Route>
+          <Route path="/settings" render={() => <Settings />}></Route>
+          <Route path="/sidebar" render={() => <Sidebar />}></Route>
+          <Route path="/login" render={() => <Login />}></Route>
+        </div>
+        <div className="footer">
+          <Route render={() => <Footer />}></Route>
+        </div>
+      </div>
+    )
+  }
 }
 
 const mapStateToProps = (state: StoreType) => ({
-    initialized: state.app.initialized,
+  initialized: state.app.initialized,
 })
 
-export default compose<React.ComponentType>(withRouter,
-    connect(mapStateToProps, {initializeAppTC}))(App);
-
+export default compose<React.ComponentType>(
+  withRouter,
+  connect(mapStateToProps, { initializeAppTC })
+)(App)
 
 //Types
 type mapDispatchToPropsType = {
-    initializeAppTC: () => void
+  initializeAppTC: () => void
 }
 type mapStateToPropsType = {
-    initialized: boolean
+  initialized: boolean
 }
-type AppPropsType = mapStateToPropsType & mapDispatchToPropsType;
+type AppPropsType = mapStateToPropsType & mapDispatchToPropsType
