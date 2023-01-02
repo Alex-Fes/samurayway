cc1a6f60bb7458933ea1f92c5b3fe2a52db39fff
Alex-Fes <aleksei.fesenko86@gmail.com>
2023-01-02 20:26:01 +0100

    style: add ESLint

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,74 +1,75 @@
-import axios from "axios";
-import {RootUserProfileType} from "../Components/Profile/RootUserProfileType";
+import axios from 'axios'
 
+import { RootUserProfileType } from '../Components/Profile/RootUserProfileType'
 
 const instance = axios.create({
-    withCredentials: true,
-    baseURL: 'https://social-network.samuraijs.com/api/1.0/',
-    headers: {
-        'API-KEY': '3af1a50e-9363-4d90-80ea-b72e392fafcb'
-    }
+  withCredentials: true,
+  baseURL: 'https://social-network.samuraijs.com/api/1.0/',
+  headers: {
+    'API-KEY': '3af1a50e-9363-4d90-80ea-b72e392fafcb',
+  },
 })
 
-
 export const usersAPI = {
-    getUsers(currentPage = 1, pageSize = 10) {
-        return instance.get(`users?page=${currentPage}&count=${pageSize}`)
-    },
-    followUser(userId: number) {
-        return instance.post(`follow/${userId}`, {})
-    },
-    unFollowUser(userId: number) {
-        return instance.delete(`follow/${userId}`)
-    },
-    getProfile(userId: string) {
-        console.warn("Obsolete method. Please use profileAPI object.")
-        return profileAPI.getProfile(userId)
-        // .then(response => response.data)
-    }
-    // authUser (id: number, email: string, login: string) {
-    //     return  instance.get(`auth/me`).then(response => response.data
-    //         // if (response.data.resultCode === 0) {
-    //         //     let {id, email, login} = response.data.data;
-    //         //     this.props.setAuthUserDataAC(id, email, login)
-    //         // }
-    //     )
-    // }
+  getUsers(currentPage = 1, pageSize = 10) {
+    return instance.get(`users?page=${currentPage}&count=${pageSize}`)
+  },
+  followUser(userId: number) {
+    return instance.post(`follow/${userId}`, {})
+  },
+  unFollowUser(userId: number) {
+    return instance.delete(`follow/${userId}`)
+  },
+  getProfile(userId: string) {
+    console.warn('Obsolete method. Please use profileAPI object.')
+
+    return profileAPI.getProfile(userId)
+    // .then(response => response.data)
+  },
+  // authUser (id: number, email: string, login: string) {
+  //     return  instance.get(`auth/me`).then(response => response.data
+  //         // if (response.data.resultCode === 0) {
+  //         //     let {id, email, login} = response.data.data;
+  //         //     this.props.setAuthUserDataAC(id, email, login)
+  //         // }
+  //     )
+  // }
 }
 
 export type ResponseType<D = {}> = {
-    resultCode: number
-    messages: Array<string>
-    data: D
+  resultCode: number
+  messages: Array<string>
+  data: D
 }
 
 export const profileAPI = {
-    getProfile(userId: string) {
-        return instance.get<RootUserProfileType>(`profile/` + userId)
-    },
-    getStatus(userId: string) {
-        return instance.get(`profile/status/` + userId)
-    },
-    updateStatus(status: string) {
-        return instance.put(`profile/status/`, {status: status})
-    }
-
+  getProfile(userId: string) {
+    return instance.get<RootUserProfileType>(`profile/` + userId)
+  },
+  getStatus(userId: string) {
+    return instance.get(`profile/status/` + userId)
+  },
+  updateStatus(status: string) {
+    return instance.put(`profile/status/`, { status: status })
+  },
 }
-
 
 export const authAPI = {
-    me() {
-        return instance.get(`auth/me`)
-        //.then(response => response.data)
-    },
-    login(email: string, password: string, rememberMe: boolean) {
-        return instance.post<ResponseType<{ userId: number }>>('/auth/login', {email, password, rememberMe})
-    },
-    logout() {
-        return instance.delete<ResponseType>('/auth/login')
-    }
+  me() {
+    return instance.get(`auth/me`)
+    //.then(response => response.data)
+  },
+  login(email: string, password: string, rememberMe: boolean) {
+    return instance.post<ResponseType<{ userId: number }>>('/auth/login', {
+      email,
+      password,
+      rememberMe,
+    })
+  },
+  logout() {
+    return instance.delete<ResponseType>('/auth/login')
+  },
 }
-
 
 // export const getUsers = (currentPage = 1, pageSize = 10) => {
 //     return instance.get(`users?page=${currentPage}&count=${pageSize}`,).then(response => response.data)
@@ -86,4 +87,3 @@ export const authAPI = {
 //     return instance.get<RootUserProfileType>(`profile/` + userId)
 //         .then(response => response.data)
 // }
-
