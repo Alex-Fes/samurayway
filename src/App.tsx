c13d38b42682354674ceefbb2966a573807ff854
Alex-Fes <aleksei.fesenko86@gmail.com>
2023-01-09 17:53:03 +0100

    refactor: use React lazy and Suspense

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 
 import './App.css'
 import { connect } from 'react-redux'
@@ -10,20 +10,26 @@ import DialogsContainer from './Components/Dialogs/DialogsContainer'
 import Footer from './Components/Footer/Footer'
 import HeaderContainer from './Components/Header/HeaderContainer'
 import Login from './Components/Login/Login'
-import Music from './Components/Music/Music'
+//import Music from './Components/Music/Music'
 import Navigation from './Components/Navigation/Navigation'
-import News from './Components/News/News'
+//import News from './Components/News/News'
 import ProfileContainer from './Components/Profile/ProfileContainer'
-import Settings from './Components/Settings/Settings'
-import Sidebar from './Components/Sidebar/Sidebar'
+//import Settings from './Components/Settings/Settings'
+//import Sidebar from './Components/Sidebar/Sidebar'
 import UsersContainer from './Components/Users/UsersContainer'
 import { initializeAppTC } from './Redux/appReducer'
 import { StoreType } from './Redux/redux-store'
+
+const Music = React.lazy(() => import('./Components/Music/Music'))
+const News = React.lazy(() => import('./Components/News/News'))
+const Settings = React.lazy(() => import('./Components/Settings/Settings'))
+const Sidebar = React.lazy(() => import('./Components/Sidebar/Sidebar'))
 
 class App extends React.Component<AppPropsType> {
   componentDidMount() {
     this.props.initializeAppTC()
   }
+
   render() {
     if (!this.props.initialized) {
       return <Preloader />
@@ -33,15 +39,16 @@ class App extends React.Component<AppPropsType> {
       <div className="app-wrapper">
         <HeaderContainer />
         <Navigation />
-
         <div className="app-wrapper-content">
           <Route path="/profile/:userId?" render={() => <ProfileContainer />}></Route>
           <Route path="/dialogs" render={() => <DialogsContainer />}></Route>
-          <Route path="/news" render={() => <News />}></Route>
-          <Route path="/music" render={() => <Music />}></Route>
+          <Suspense fallback={<div>Loading...</div>}>
+            <Route path="/news" render={() => <News />}></Route>
+            <Route path="/music" render={() => <Music />}></Route>{' '}
+            <Route path="/settings" render={() => <Settings />}></Route>
+            <Route path="/sidebar" render={() => <Sidebar />}></Route>
+          </Suspense>
           <Route path="/users" render={() => <UsersContainer />}></Route>
-          <Route path="/settings" render={() => <Settings />}></Route>
-          <Route path="/sidebar" render={() => <Sidebar />}></Route>
           <Route path="/login" render={() => <Login />}></Route>
         </div>
         <div className="footer">
