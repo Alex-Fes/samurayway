12865d0f19123878fa77770e318e3957422a8348
Alex-Fes <aleksei.fesenko86@gmail.com>
2023-01-11 17:59:34 +0100

    refactor: change Types of validators

--- src/utilits/validators/validators.tsx
+++ src/utilits/validators/validators.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
 
-export const required = (value: string) => (value ? undefined : 'Field is required')
+export const required: FieldValidatorType = value => (value ? undefined : 'Field is required')
 
-export const maxLengthCreator = (maxLength: number) => (value: string) => {
-  if (value.length > maxLength) return `Max length is ${maxLength} symbols`
+export const maxLengthCreator =
+  (maxLength: number): FieldValidatorType =>
+  value => {
+    if (value.length > maxLength) return `Max length is ${maxLength} symbols`
 
-  return undefined
-}
+    return undefined
+  }
+
+//TYPES=============================
+export type FieldValidatorType = (value: string) => string | undefined
