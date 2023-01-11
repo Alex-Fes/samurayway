8ff5b0e17384d1c96a1d18b74365cae91bf63f9a
Alex-Fes <aleksei.fesenko86@gmail.com>
2023-01-11 17:55:48 +0100

    add: ProfileDataFormReduxForm and ProfileDataForm for edit profile

---
+++ src/Components/Profile/ProfileInfo/ProfileDataForm.tsx
@@ -0,0 +1,61 @@
+import React from 'react'
+
+import { InjectedFormProps, reduxForm } from 'redux-form'
+
+import { createField, Input, Textarea } from '../../common/FormsControls/FormsControl'
+
+import { ProfileDataFormPropsType } from './ProfileInfo'
+import s from './ProfileInfo.module.css'
+
+export const ProfileDataForm: React.FC<
+  InjectedFormProps<ProfileDataFormReduxFormType, ProfileDataFormPropsType> &
+    ProfileDataFormPropsType
+> = ({ initialValues, handleSubmit, error }) => {
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>{createField('Full name', 'fullName', Input, [])}</div>
+      <div>
+        <b>Looking for a job:</b>
+        {createField('', 'lookingForAJob', Input, [], { type: 'checkbox' })}
+      </div>
+      <div>
+        <b>My professional skills:</b>
+        {createField('My professional skills', 'lookingForAJobDescription', Textarea, [])}
+      </div>
+      <span>About me:</span>
+      {createField('About me', 'aboutMe', Textarea, [])}
+      <div>
+        <b>Contacts:</b>
+        <ul>
+          {Object.keys(initialValues.contacts).map(key => {
+            return (
+              <li key={key}>
+                <b>{key}</b> : {createField(key, 'contacts.' + key, Textarea, [])}
+              </li>
+            )
+          })}
+        </ul>
+      </div>
+      {error && <div className={s.formError}>{error}</div>}
+      <div>
+        <button>save</button>
+      </div>
+    </form>
+  )
+}
+
+export const ProfileDataFormReduxForm = reduxForm<
+  ProfileDataFormReduxFormType,
+  ProfileDataFormPropsType
+>({
+  form: 'edit-profile',
+})(ProfileDataForm)
+
+//TYPES========================================
+
+export type ProfileDataFormReduxFormType = {
+  lookingForAJob: boolean
+  aboutMe: string
+  lookingForAJobDescription: string
+  fullName: string
+}
