121bb2bab00317e1e58660ef48b27e485163ad57
Alex-Fes <aleksei.fesenko86@gmail.com>
2023-01-11 19:14:49 +0100

    add: create securityAPI

--- src/api/api.ts
+++ src/api/api.ts
@@ -61,15 +61,22 @@ export const authAPI = {
   me() {
     return instance.get(`auth/me`)
   },
-  login(email: string, password: string, rememberMe: boolean) {
+  login(email: string, password: string, rememberMe: boolean, captcha: string) {
     return instance.post<ResponseType<{ userId: number }>>('/auth/login', {
       email,
       password,
       rememberMe,
+      captcha,
     })
   },
   logout() {
     return instance.delete<ResponseType>('/auth/login')
+  },
+}
+
+export const securityAPI = {
+  getCaptchaUrl() {
+    return instance.get(`security/get-captcha-url`)
   },
 }
 
