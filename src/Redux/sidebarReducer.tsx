4da971f59a9b8ef5c339fd4fb9f36780b6988b8a
Alex-Fes <aleksei.fesenko86@gmail.com>
2023-01-20 16:32:36 +0100

    delete sidebarReducer.tsx

--- src/Redux/sidebarReducer.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-import { ActionTypes, SidebarType } from './state'
-
-let initialState = {}
-
-export const sidebarReducer = (state: SidebarType = initialState, action: ActionTypes) => {
-  return state
-}
