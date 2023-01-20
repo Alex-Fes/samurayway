6250c772887543907b88af2a9b633800291abf01
Alex-Fes <aleksei.fesenko86@gmail.com>
2023-01-20 16:21:23 +0100

    refactor: add status to app

--- src/App/appReducer.ts
+++ src/App/appReducer.ts
@@ -1,18 +1,21 @@
 import { Dispatch } from 'redux'
 
-import { getAuthUserDataTC } from './authReducer'
-import { AppThunkType } from './redux-store'
-import { ActionTypes } from './state'
+import { getAuthUserDataTC } from '../features/auth/authReducer'
+import { AppActionsType } from '../Redux/types'
 
-const INITIALIZED_SUCCESS = 'INITIALIZED-SUCCESS'
+import { AppThunkType } from './store'
+
+const INITIALIZED_SUCCESS = 'app/INITIALIZED-SUCCESS'
+const SET_APP_STATUS = 'app/SET-STATUS'
 
 let initialState: InitialStateType = {
   initialized: false,
+  status: 'idle',
 }
 
 export const appReducer = (
   state: InitialStateType = initialState,
-  action: ActionTypes
+  action: AppActionsType
 ): InitialStateType => {
   switch (action.type) {
     case INITIALIZED_SUCCESS:
@@ -20,16 +23,26 @@ export const appReducer = (
         ...state,
         initialized: true,
       }
+    case SET_APP_STATUS:
+      return {
+        ...state,
+        status: action.status,
+      }
     default:
       return state
   }
 }
 export const initializedSuccessAC = () => ({ type: INITIALIZED_SUCCESS } as const)
 
+export const setAppStatusAC = (status: RequestStatusType) =>
+  ({ type: SET_APP_STATUS, status } as const)
+
 export const initializeAppTC = (): AppThunkType => {
   return (dispatch: Dispatch) => {
+    dispatch(setAppStatusAC('loading'))
     dispatch(getAuthUserDataTC()).then(() => {
       dispatch(initializedSuccessAC())
+      dispatch(setAppStatusAC('succeeded'))
     })
   }
 }
@@ -37,4 +50,6 @@ export const initializeAppTC = (): AppThunkType => {
 //Types
 export type InitialStateType = {
   initialized: boolean
+  status: RequestStatusType
 }
+export type RequestStatusType = 'idle' | 'loading' | 'succeeded' | 'failed'
