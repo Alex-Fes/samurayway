214650a145b1825af0c4646276ed0e52f795ecce
Alex-Fes <aleksei.fesenko86@gmail.com>
2023-01-22 21:53:15 +0100

    refactor: add Paper to Sidebar.tsx

--- src/Components/Sidebar/Sidebar.tsx
+++ src/Components/Sidebar/Sidebar.tsx
@@ -1,7 +1,15 @@
 import React from 'react'
 
+import { Paper } from '@mui/material'
+
 const Sidebar = () => {
-  return <div> My Friends</div>
+  return (
+    <div>
+      <Paper elevation={3} sx={{ width: '100%', height: '300px' }}>
+        My Friends
+      </Paper>
+    </div>
+  )
 }
 
 export default Sidebar
