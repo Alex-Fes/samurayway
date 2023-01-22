bf8b2b3d72a7097c49119487dfe8a338b2fb25da
Alex-Fes <aleksei.fesenko86@gmail.com>
2023-01-22 21:50:32 +0100

    refactor: add Paper to News.tsx

--- src/Components/News/News.tsx
+++ src/Components/News/News.tsx
@@ -1,7 +1,15 @@
 import React from 'react'
 
+import { Paper } from '@mui/material'
+
 const News = () => {
-  return <div>News</div>
+  return (
+    <div>
+      <Paper elevation={3} sx={{ width: '100%', height: '300px', marginTop: '20px' }}>
+        News
+      </Paper>
+    </div>
+  )
 }
 
 export default News
