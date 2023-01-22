9a38cd2f12bd56eebb3de5711cda650f0483defd
Alex-Fes <aleksei.fesenko86@gmail.com>
2023-01-22 21:49:40 +0100

    refactor: add new value to props

--- src/Components/Profile/MyPosts/Post/MyPostsContainer.tsx
+++ src/Components/Profile/MyPosts/Post/MyPostsContainer.tsx
@@ -7,48 +7,6 @@ import { StoreType } from '../../../../App/store'
 import { addPostActionCreator } from '../../../../Redux/profileReducer'
 import MyPosts from '../MyPosts'
 
-// type MyPostsPropsType = {
-//     // newPostText: string
-//     // posts: PostType[]
-//     // dispatch: (action: ActionTypes) => void
-//     // store: any
-// }
-
-// const MyPostsContainer = () => {
-//     // let state = props.store.getState();
-//     // let postsElement = state.posts.map(p => <Post message={p.message} likeCount={p.likeCount} key={p.id}/>)
-//     // let newPostElement = React.createRef<HTMLTextAreaElement>();
-//     // let addPost = () => {
-//     //     // if (newPostElement.current) {
-//     //     // props.addPostCallback(newPostElement.current.value)
-//     //     // props.dispatch({type: 'ADD-POST', newPost: newPostElement.current.value})
-//     //     props.store.dispatch(addPostActionCreator(state.ProfilePage.newPostText))
-//     //     // newPostElement.current.value = '';
-//     // }
-//     // const onChangePost = (text: string) => {
-//     //     // props.dispatch({type: 'CHANGE-NEW-TEXT', newText: e.currentTarget.value})
-//     //    props.store.dispatch(onChangePostActionCreator(text));
-//     //     // props.changeNewTextCallback('')
-//     // }
-//     return (
-//         <StoreContext.Consumer>
-//             {(store) => {
-//                 let state = store.getState();
-//                 let addPost = () => {
-//                     store.dispatch(addPostActionCreator(state.ProfilePage.newPostText))
-//                 }
-//                 const onChangePost = (text: string) => {
-//                     store.dispatch(onChangePostActionCreator(text));
-//                 }
-//                 return <MyPosts onChangePost={onChangePost}
-//                                 addPost={addPost}
-//                                 posts={state.ProfilePage.posts}
-//                                 newPostText={state.ProfilePage.newPostText}/>
-//             }}
-//         </StoreContext.Consumer>
-//     )
-// }
-
 type PostType = {
   id: number
   message: string
@@ -56,10 +14,10 @@ type PostType = {
 }
 type MapStateToPropsType = {
   posts: Array<PostType>
-  // newPostText: string
+  smallAva: string
+  fullName: string
 }
 type MapDispatchToPropsType = {
-  // onChangePost: (text: string)=>void
   addPost: (newPostText: string) => void
 }
 export type MyPostPropsType = MapStateToPropsType & MapDispatchToPropsType
@@ -67,14 +25,12 @@ export type MyPostPropsType = MapStateToPropsType & MapDispatchToPropsType
 const mapStateToProps = (state: StoreType): MapStateToPropsType => {
   return {
     posts: state.ProfilePage.posts,
-    // newPostText: state.ProfilePage.newPostText
+    smallAva: state.ProfilePage.profile.photos.small,
+    fullName: state.ProfilePage.profile.fullName,
   }
 }
 const mapDispatchToProps = (dispatch: Dispatch): MapDispatchToPropsType => {
   return {
-    // onChangePost: (text:string) => {
-    //     dispatch(onChangePostActionCreator(text))
-    // },
     addPost: (newPostText: string) => {
       dispatch(addPostActionCreator(newPostText))
     },
