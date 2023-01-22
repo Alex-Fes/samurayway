0e327bd7faa7dcb928ec2deff6917cb153d884fb
Alex-Fes <aleksei.fesenko86@gmail.com>
2023-01-22 21:47:30 +0100

    create: HomePage.tsx

---
+++ src/Components/HomePage/HomePage.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+import { Grid } from '@mui/material'
+
+import News from '../News/News'
+import MyPostsContainer from '../Profile/MyPosts/Post/MyPostsContainer'
+import ProfileContainer from '../Profile/ProfileContainer'
+import Sidebar from '../Sidebar/Sidebar'
+
+import s from './HomePage.module.scss'
+export const HomePage = () => {
+  return (
+    <div className={s.mainContainer}>
+      <Grid container spacing={3}>
+        <Grid item xs={3}>
+          <ProfileContainer />
+        </Grid>
+        <Grid item xs={6}>
+          <MyPostsContainer />
+        </Grid>
+        <Grid item xs={3} spacing={2}>
+          <Sidebar />
+
+          <News />
+        </Grid>
+      </Grid>
+    </div>
+  )
+}
