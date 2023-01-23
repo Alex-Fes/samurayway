5c7189eec7fad3d8c250ccebcb7f0bbc63b0335e
Alex-Fes <aleksei.fesenko86@gmail.com>
2023-01-23 16:50:04 +0100

    create: EditPostMenu.tsx for posts

---
+++ src/Components/Profile/MyPosts/Post/EditPostMenu.tsx
@@ -0,0 +1,45 @@
+import * as React from 'react'
+
+import MoreVertIcon from '@mui/icons-material/MoreVert'
+import IconButton from '@mui/material/IconButton'
+import Menu from '@mui/material/Menu'
+import MenuItem from '@mui/material/MenuItem'
+
+export default function EditPostMenu() {
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null)
+  const open = Boolean(anchorEl)
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget)
+  }
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
+
+  return (
+    <div>
+      <IconButton
+        aria-label="more"
+        id="long-button"
+        aria-controls={open ? 'long-menu' : undefined}
+        aria-expanded={open ? 'true' : undefined}
+        aria-haspopup="true"
+        onClick={handleClick}
+      >
+        <MoreVertIcon />
+      </IconButton>
+
+      <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          'aria-labelledby': 'basic-button',
+        }}
+      >
+        <MenuItem onClick={handleClose}>Edit post</MenuItem>
+        <MenuItem onClick={handleClose}>Delete post</MenuItem>
+      </Menu>
+    </div>
+  )
+}
