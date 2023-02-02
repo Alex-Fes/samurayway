8dbe3902465e22f9435c44556926b2e59f6519c1
Alex-Fes <aleksei.fesenko86@gmail.com>
2023-02-02 13:22:17 +0100

    refactor: add markup to form

--- src/features/auth/Login.tsx
+++ src/features/auth/Login.tsx
@@ -5,12 +5,12 @@ import { Redirect } from 'react-router-dom'
 import { InjectedFormProps, reduxForm } from 'redux-form'
 
 import { StoreType } from '../../App/store'
-import styles from '../../Components/common/FormsControls/FormControls.module.css'
+import styles from '../../Components/common/FormsControls/FormControls.module.scss'
 import { createField, Input } from '../../Components/common/FormsControls/FormsControl'
 import { maxLengthCreator, required } from '../../utilits/validators/validators'
 
 import { loginUserTC } from './authReducer'
-
+import s from './Login.module.scss'
 const maxLengthForLogin = maxLengthCreator(50)
 const maxLengthForPassword = maxLengthCreator(20)
 
@@ -19,28 +19,40 @@ export const LoginForm: React.FC<
 > = ({ handleSubmit, error, captchaUrl }) => {
   return (
     <form onSubmit={handleSubmit}>
-      {createField<LoginFormValuesTypeKeys>('email', 'email', Input, [required, maxLengthForLogin])}
-      {createField<LoginFormValuesTypeKeys>(
-        'Password',
-        'password',
-        Input,
-        [required, maxLengthForPassword],
-        {
-          type: 'password',
-        }
-      )}
-      {createField<LoginFormValuesTypeKeys>(
-        'checkbox',
-        'rememberMe',
-        Input,
-        [],
-        { type: 'checkbox' },
-        'remember me'
-      )}
-      {captchaUrl && <img src={captchaUrl} alt="captcha" />}
-      {captchaUrl &&
-        createField<LoginFormValuesTypeKeys>('Symbols from image', 'captcha', Input, [required])}
-      {error && <div className={styles.formSummaryError}>{error}</div>}
+      <div>
+        {createField<LoginFormValuesTypeKeys>('email', 'email', Input, [
+          required,
+          maxLengthForLogin,
+        ])}
+      </div>
+      <div>
+        {createField<LoginFormValuesTypeKeys>(
+          'Password',
+          'password',
+          Input,
+          [required, maxLengthForPassword],
+          {
+            type: 'password',
+          }
+        )}
+      </div>
+      <div>
+        {createField<LoginFormValuesTypeKeys>(
+          'checkbox',
+          'rememberMe',
+          Input,
+          [],
+          { type: 'checkbox' },
+          'remember me'
+        )}
+      </div>
+      <div>{captchaUrl && <img src={captchaUrl} alt="captcha" />}</div>
+      <div>
+        {captchaUrl &&
+          createField<LoginFormValuesTypeKeys>('Symbols from image', 'captcha', Input, [required])}
+      </div>
+      <div>{error && <div className={styles.formSummaryError}>{error}</div>}</div>
+
       <div>
         <button>Login</button>
       </div>
@@ -56,6 +68,7 @@ const mapStateToProps = (state: StoreType): MapStateToPropsType => ({
   captchaUrl: state.auth.captchaUrl,
   isAuth: state.auth.isAuth,
 })
+
 const Login = (props: LoginPropsType) => {
   const onSubmit = (formData: LoginFormValuesType) => {
     props.loginUserTC(formData.email, formData.password, formData.rememberMe, formData.captcha)
@@ -66,9 +79,32 @@ const Login = (props: LoginPropsType) => {
   }
 
   return (
-    <div>
-      <h1>LOGIN</h1>
-      <LoginReduxForm onSubmit={onSubmit} captchaUrl={props.captchaUrl} />
+    <div className={s.container}>
+      <div className={s.loginContainer}>
+        <div className={s.descriptionBox}>
+          <div className={s.descriptionTitle}>
+            <img src="../../assets/images/logo.png" alt="logo" />
+            <h2>Samurai Way</h2>
+          </div>
+          <div className={s.description}>
+            <span>
+              To log in get registered{' '}
+              <a href="https://social-network.samuraijs.com/signUp">here</a>
+            </span>
+            <span>or use common test account credentials:</span>
+            <span>
+              Email: <b>free@samuraijs.com</b>
+            </span>
+            <span>
+              Password: <b>free</b>
+            </span>
+          </div>
+        </div>
+        <div className={s.loginBox}>
+          <h3>Sign in</h3>
+          <LoginReduxForm onSubmit={onSubmit} captchaUrl={props.captchaUrl} />
+        </div>
+      </div>
     </div>
   )
 }
