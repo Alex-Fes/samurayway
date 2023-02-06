31a412083f1d5dbf527ba3529532984acca3b8f2
Alex-Fes <aleksei.fesenko86@gmail.com>
2023-02-06 16:08:35 +0100

    refactor: change marcup in Sidebar.tsx

--- src/features/Sidebar/Sidebar.tsx
+++ src/features/Sidebar/Sidebar.tsx
@@ -1,15 +1,7 @@
 import React from 'react'
 
-import { Paper } from '@mui/material'
-
 const Sidebar = () => {
-  return (
-    <div>
-      <Paper elevation={3} sx={{ width: '100%', height: '300px' }}>
-        My Friends
-      </Paper>
-    </div>
-  )
+  return <div>My Friends</div>
 }
 
 export default Sidebar
