6d01cc647a0cad2007b26f87623f3f1b27dccd97
Alex-Fes <aleksei.fesenko86@gmail.com>
2023-02-06 16:00:47 +0100

    delete: clean all code

--- src/features/Dialogs/DialogsContainer.tsx
+++ src/features/Dialogs/DialogsContainer.tsx
@@ -9,52 +9,12 @@ import { withAuthRedirect } from '../../utilits/hoc/withAuthRedirect'
 
 import Dialogs from './Dialogs'
 
-// type DialogsPropsType = {
-//     // dialog: DialogType[]
-//     // message: MessageType[]
-//     // newMessageText: string
-//     // dispatch: (action: ActionTypes) => void
-//     // store: any
-// }
-
-// const DialogsContainer = () => {
-//     //let state = store.getState();
-//     // let dialogElements = props.dialog.map(d => <DialogItem key={d.id} name={d.name} id={d.id}/>);
-//     // let messagesElements = props.message.map(m => <Message key={m.id} id={m.id} message={m.message}/>);
-//     return (
-//         <StoreContext.Consumer>
-//             {(store) => {
-//                 let state = store.getState();
-//                 let sendMessage = () => {
-//                     //props.addMessageCallback(newMessage.current.value)
-//                     store.dispatch(sendMessageActionCreator(state.DialogsPage.newMessageText))
-//                 }
-//                 let addMessage = (newMessageTextBody: string) => {
-//                     //props.changeNewMessageTextCallback(e.currentTarget.value)
-//                     //  props.dispatch({type: 'CHANGE-NEW-MESSAGE-TEXT', newMessage: e.currentTarget.value})
-//                     store.dispatch(addMessageActionCreator(newMessageTextBody))
-//                 }
-//                 return <Dialogs dialog={state.DialogsPage.dialogs}
-//                                 message={state.DialogsPage.message}
-//                                 sendMessage={sendMessage}
-//                                 newMessageText={state.DialogsPage.newMessageText}
-//                                 changeNewMessageTextCallback={addMessage}/>
-//             }}
-//         </StoreContext.Consumer>
-//     )
-// }
-
 type MapStateToPropsType = {
   dialogs: Array<DialogType>
   message: Array<MessageType>
-  //newMessageText: string
-
-  //isAuth: boolean - авторизация будет браться  в HOC
 }
 type MapDispatchToPropsType = {
-  //onSubmit: ()=> void
   sendMessage: (newMessageText: string) => void
-  //changeNewMessageTextCallback: (newMessageTextBody: string) => void
 }
 export type DialogsPropsType = MapDispatchToPropsType & MapStateToPropsType
 
@@ -62,8 +22,6 @@ const mapStateToProps = (state: StoreType): MapStateToPropsType => {
   return {
     dialogs: state.DialogsPage.dialogs,
     message: state.DialogsPage.message,
-    // newMessageText: state.DialogsPage.newMessageText,
-    //isAuth: state.auth.isAuth - авторизация будет браться  в HOC
   }
 }
 const mapDispatchToProps = (dispatch: Dispatch): MapDispatchToPropsType => {
@@ -71,14 +29,8 @@ const mapDispatchToProps = (dispatch: Dispatch): MapDispatchToPropsType => {
     sendMessage: (newMessage: string) => {
       dispatch(sendMessageActionCreator(newMessage))
     },
-    // changeNewMessageTextCallback: (newMessageTextBody: string) => {
-    //     dispatch(addMessageActionCreator(newMessageTextBody))
   }
 }
-
-// let AuthRedirectComponent = withAuthRedirect(Dialogs)
-//
-// const DialogsContainer = connect(mapStateToProps, mapDispatchToProps)(AuthRedirectComponent)
 
 export default compose<React.ComponentType>(
   connect(mapStateToProps, mapDispatchToProps),
