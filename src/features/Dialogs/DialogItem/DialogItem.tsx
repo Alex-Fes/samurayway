38df0e069c1afdbb142850786d37c673a09f9594
Alex-Fes <aleksei.fesenko86@gmail.com>
2023-02-06 15:57:23 +0100

    refactor: change style name

--- src/features/Dialogs/DialogItem/DialogItem.tsx
+++ src/features/Dialogs/DialogItem/DialogItem.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 import { NavLink } from 'react-router-dom'
 
-import style from './../Dialogs.module.css'
-
+import userPic from '../../../assets/images/user-circle-icon-png.png'
+import style from '../Dialogs.module.scss'
 type DialogPropsType = {
   name: string
   id: number
@@ -13,7 +13,8 @@ const DialogItem = (props: DialogPropsType) => {
   let path = '/dialogs/' + props.id
 
   return (
-    <div className={style.dialog + ' ' + style.active}>
+    <div className={style.dialogItem + ' ' + style.active}>
+      <img src={userPic} alt="" />
       <NavLink to={path}>{props.name}</NavLink>
     </div>
   )
