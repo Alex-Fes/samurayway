3780019fb0850b329b8134f0084a9c3f68349537
Alex-Fes <aleksei.fesenko86@gmail.com>
2023-02-06 16:03:07 +0100

    refactor: change markup in HomePage.tsx

--- src/features/HomePage/HomePage.tsx
+++ src/features/HomePage/HomePage.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 
-import { Grid } from '@mui/material'
+import { Grid, Paper } from '@mui/material'
 
-import News from '../News/News'
+import { SvgSelector } from '../../common/svgSelector/SvgSelector'
 import MyPostsContainer from '../Profile/MyPosts/MyPostsContainer'
 import ProfileContainer from '../Profile/ProfileContainer'
-import Sidebar from '../Sidebar/Sidebar'
 
 import s from './HomePage.module.scss'
+
 export const HomePage = () => {
   return (
     <div className={s.mainContainer}>
@@ -19,9 +19,23 @@ export const HomePage = () => {
           <MyPostsContainer />
         </Grid>
         <Grid item xs={3} spacing={2}>
-          <Sidebar />
+          <Paper elevation={3} sx={{ width: '100%', height: '300px', marginBottom: '20px' }}>
+            <div className={s.friendsContainer}>
+              <h3>My friends</h3>
+              <SvgSelector svgname={'underDevelopment'} />
+              <p>This page is under development.</p>
+            </div>
+            {/*<Sidebar />*/}
+          </Paper>
 
-          <News />
+          <Paper elevation={3} sx={{ width: '100%', height: '300px' }}>
+            <div className={s.newsContainer}>
+              <h3>News</h3>
+              <SvgSelector svgname={'underDevelopment'} />
+              <p>This page is under development.</p>
+              {/*<News />*/}
+            </div>
+          </Paper>
         </Grid>
       </Grid>
     </div>
